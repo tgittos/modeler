bb30eafb594952918d4a5bc099729d9996a2d72b
Tim <tim@chromasplosion.com>
2011-06-07 21:58:38 -0500

    Stubbing out some unit tests

---
+++ test/tests/redback/core/WebGLObject.js
@@ -0,0 +1,16 @@
+module("WebGLObject");
+
+test('initialization sets vertices', function(){
+});
+
+test('setting vertices in initialization generates lines', function(){
+  
+});
+
+test('initialization sets indices', function(){
+  
+});
+
+test('initialization sets materials', function(){
+  
+});
