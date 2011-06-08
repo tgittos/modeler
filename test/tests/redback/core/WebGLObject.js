aaa74821fb45c24c8dc64fd43ed5e721f9443493
Tim <tim@chromasplosion.com>
2011-06-08 16:55:30 -0500

    Updated unit test stubs, removed irrelevant tests from the runner, added new tests to the runner

--- test/tests/redback/core/WebGLObject.js
+++ test/tests/redback/core/WebGLObject.js
@@ -1,16 +1,45 @@
 module("WebGLObject");
 
 test('initialization sets vertices', function(){
+  throw('not implemented');
 });
 
 test('setting vertices in initialization generates lines', function(){
-  
+  throw('not implemented');
 });
 
 test('initialization sets indices', function(){
-  
+  throw('not implemented');
 });
 
 test('initialization sets materials', function(){
-  
+  throw('not implemented');
+});
+
+test('can set vertices', function() {
+  throw('not implemented');
+});
+
+test('can set indices', function(){
+  throw('not implemented');
+});
+
+test('can set materials', function(){
+  throw('not implemented');
+});
+
+test('can get vertices', function(){
+  throw('not implemented');
+});
+
+test('can get indices', function(){
+  throw('not implemented');
+});
+
+test('can get lines', function(){
+  throw('not implemented');
+});
+
+test('can get materials', function(){
+  throw('not implemented');
 });
